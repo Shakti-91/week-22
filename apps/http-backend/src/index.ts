9f1483037f5739b9fe53738c958f7d4c7b7a056f
shakti <singhrathoreshakti47@gmail.com>
2026-03-03 12:32:32 +0000

    finaaly database chal gya yayy

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken"
 import {JWT_SECRET} from "@repo/backend-common/secret" 
 import { auth } from "./middleware.js";
 import {CreateUserSchema,SignInSchema,RoomSchema} from "@repo/common/zodSchem"
+import {prismaClient} from "@repo/db/client"
 const app=express();
 app.use(express.json());
 
@@ -27,13 +28,24 @@ app.post('/signin',(req,res)=>{
     return res.json({token:token});
 })
 
-app.get('/hi',auth,(req,res)=>{
-    const data=RoomSchema.safeParse(req.body);
+app.get('/hi',async(req,res)=>{
+      try {
+    const user = await prismaClient.user.create({
+      data: {
+        name: "shakti",   // or simply name
+      },
+    });
 
-    if(!data.success){
-        return res.status(411).json({message:"invalid input"})
-    }
-    return res.json({message:"hi"})
+    console.log("User created:", user);
+    return res.json({message:"kuch to huwa"})
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return res.json({message:"error na jare "})
+}
+})
+
+app.post("/test",(req,res)=>{
+    return res.json({message:"hi there"})
 })
 
 app.listen(3001)
